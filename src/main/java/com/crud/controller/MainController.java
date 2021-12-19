82d2c792680462d07edcbb7d241928534181e362
sunderkrishnaupreti <sunderkrishna_upreti@yahoo.com>
2021-12-19 16:28:33 -0600

    rest controller added for the crud operations

--- src/main/java/com/crud/controller/MainController.java
+++ src/main/java/com/crud/controller/MainController.java
@@ -4,9 +4,7 @@ import java.util.List;
 
 import com.crud.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.crud.entity.Employee;
 import com.crud.repository.EmployeeRepo;
@@ -18,11 +16,26 @@ public class MainController {
 	@Autowired
 	EmployeeService employeeService;
 	
-//	//this function returns all the employees
-//	@GetMapping("/employees")
-//	public List<Employee> getAllEmployees(){
-//
-//
-//	}
+	//this function returns all the employees
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees(){
+		return  employeeService.getAllEmployees();
+	}
+
+	@PostMapping("/add-employee")
+	public Employee addEmployee(Employee employee){
+		return employeeService.saveEmployee(employee);
+	}
+
+	@GetMapping("/get-employee/{id}")
+	public Employee getEmployeeById(@PathVariable Long id){
+
+		return employeeService.getEmployeeById(id);
+	}
+
+	@GetMapping("/delete-employee/{id}")
+	public void deleteEmployeeById(@PathVariable Long id){
+		employeeService.deleteEmloyeeById(id);
+	}
 
 }
